4d17744c04fce730dd524258b2a72e4bb023c710
Atishaye Jain <64211411+atishaye@users.noreply.github.com>
2021-04-05 01:40:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import requests, json, telebot, time, sys, get_live_matches, get_live_score, info
+import get_live_matches, get_live_score, info
 
 token = info.token
 bot = info.bot
@@ -8,9 +8,10 @@ def send_welcome(message):
 	bot.reply_to(message, "Howdy, how are you doing?")
 	global chat_id
 	chat_id = message.chat.id
-	msg = bot.reply_to(message, "Welcome to test project\nEnter the match number whose updates you want to receive")
+	msg = bot.send_message(chat_id,
+		"Welcome to Test project\nEnter the match number whose updates you want to receive")
 	ongoing_matches = get_live_matches.live_matches()
-	info.send_msg(ongoing_matches, chat_id)
+	bot.send_message(chat_id, ongoing_matches)
 	bot.register_next_step_handler(msg, get_live_score.fetch_score)
 
-bot.polling()
+bot.polling(none_stop = True)
